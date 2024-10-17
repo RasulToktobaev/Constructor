c273e75f166f514096804dcf142be7c363306998
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-10-17 22:43:52 +0600

    add values

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const model = [
     { type: 'title', value: 'Hello World from Js' },
-    { type: 'text', value: 'This is text in second block' }
+    { type: 'text', value: 'This is text in second block' },
+    {
+        type: 'columns', value: [
+            '111111',
+            '222222',
+            '333333'
+        ]
+    }
 ]
