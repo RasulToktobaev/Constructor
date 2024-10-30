8368ab8d0d0eeb8c7b1b0c235946693707b945ff
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-10-30 23:08:56 +0600

    description of styles as an object

--- src/model.js
+++ src/model.js
@@ -5,7 +5,13 @@ export const model = [
     {
         type: 'title', value: 'Конструктор сайтов ', options: {
             tag: 'h2',
-            styles: `background: linear-gradient(90deg, #0000ff, #8a2be2); color: #fff; text-align: center; padding:1.5rem`
+            //styles: `background: linear-gradient(90deg, #0000ff, #8a2be2); color: #fff; text-align: center; padding:1.5rem`
+            styles: {
+                background: 'linear-gradient(90deg, #0000ff, #8a2be2)',
+                color: '#fff',
+                textAlign: 'center',
+                padding: '1.5rem'
+            }
         }
     },
     { type: 'text', value: 'This is text in second block' },
