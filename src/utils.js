75b952275f419400ba2f1a36bd5e18e15f4a4fbd
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-11-12 21:53:39 +0600

    add new function

--- src/utils.js
+++ src/utils.js
@@ -7,9 +7,8 @@ export function col(content) {
 }
 
 export function css(styles = {}) {
-
-
     const toString = key => `${key}: ${styles[key]}`
     return Object.keys(styles).map(toString).join(';')
 
 }
+
